789a88efb87498d484d92f4b381002f4a0a187b4
forxtoisate <hwoolford@gmail.com>
2024-01-09 14:01:39 -0600

    added the put route in category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -44,14 +44,27 @@ router.post('/', async (req, res) => {
   const categoryData = await Category.create(req.body);
   res.status(200).json(categoryData);
  } catch (err) {
-  console.error(err)
+  console.error(err);
   res.status(400).json(err);
  }
 });
 
 // update a category by its `id` value
-router.put('/:id', (req, res) => {
-
+router.put('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category with this id'});
+      return;
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
 
 // delete a category by its `id` value
